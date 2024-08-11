968567c8813f4aa5a2a537b1310a87373ea27b3f
NataliiaSurin <surin.natalia@gmail.com>
2024-08-11 15:00:17 +0200

    openpyxl_start

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("hello")
+import openpyxl as op
+df=op.load_workbook('Courses.xlsx')
+data=df.active
+total_rows=data.max_row
+#columns=data.max_column
+print(total_rows)
+for row_number in range(3,total_rows+1):
+    cell_obj = data.cell(row=row_number, column=1)
+    word=cell_obj.value
+    print(word)
+
